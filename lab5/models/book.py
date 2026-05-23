947991875037b3a9733d46532064821dd5bb4ee4
Devorius-Nagara <adrian.kotiv@gmail.com>
2026-05-23 17:22:22 +0300

    Lab 5: Flask REST API з Flasgger (Swagger UI)

---
+++ lab5/models/book.py
@@ -0,0 +1,28 @@
+from typing import List, Dict
+
+books_db: List[Dict] = [
+    {
+        "id": "a1b2c3d4-0001-0001-0001-000000000001",
+        "title": "Kobzar",
+        "author": "Taras Shevchenko",
+        "description": "A collection of Ukrainian poetry",
+        "status": "available",
+        "year": 1840,
+    },
+    {
+        "id": "a1b2c3d4-0002-0002-0002-000000000002",
+        "title": "Eneyida",
+        "author": "Ivan Kotlyarevsky",
+        "description": "Ukrainian literary classic",
+        "status": "available",
+        "year": 1798,
+    },
+    {
+        "id": "a1b2c3d4-0003-0003-0003-000000000003",
+        "title": "Lisova pisnia",
+        "author": "Lesia Ukrainka",
+        "description": "A drama-feature in verse",
+        "status": "issued",
+        "year": 1911,
+    },
+]
